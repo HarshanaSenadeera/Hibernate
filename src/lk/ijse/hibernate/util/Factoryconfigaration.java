e8223ef7e3323f4890db638db272a87c3065491c
HarshanaSenadeera <harshanasenadeera64@gmail.com>
2023-03-01 12:38:57 +0530

    Create configuration

--- src/lk/ijse/hibernate/util/Factoryconfigaration.java
+++ src/lk/ijse/hibernate/util/Factoryconfigaration.java
@@ -1,15 +1,24 @@
 package lk.ijse.hibernate.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Factoryconfigaration {
 
     private static Factoryconfigaration factoryconfigaration;
 
+    private SessionFactory sessionFactory;
+
     private Factoryconfigaration(){
 
+        Configuration configuration = new Configuration();
+        sessionFactory = configuration.buildSessionFactory();
     }
 
     public static Factoryconfigaration getInstance(){
         return (factoryconfigaration==null) ? factoryconfigaration=new Factoryconfigaration():
                 factoryconfigaration;
     }
+
+
 }
